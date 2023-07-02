5a63e383827b7145af043cf24784c85de271aa13
ozgurozbekuk <ozgurozbek.uk@gmail.com>
2023-07-02 13:20:25 +0300

    random-quate-generator
    Random quate generator with React.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from "react"
 import './App.css';
 
 function App() {
+
+  const[quates,setQuates] =useState("")
+  const getQuate = () => {
+    fetch("https://type.fit/api/quotes")
+    .then((res) => res.json())
+    .then((data) => {
+      let randomNum = Math.floor(Math.random()*data.length)
+      setQuates(data[randomNum])
+    })
+  }
+
+  useEffect(() => {
+    getQuate();
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quote">
+        <p>{quates.text}</p>
+        <p>{quates.author}</p>
+
+        <div className="btnContainer">
+          <button onClick={getQuate} className="btn">Get Quote</button>
+        </div>
+      </div>
     </div>
   );
 }
